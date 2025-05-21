26c05463f02716d111664284f15b287bcd9c7b95
Anuj Parihar <131395094+Anuj-Parihar@users.noreply.github.com>
2025-05-21 23:58:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Display longest name/display-longest-name.java
@@ -0,0 +1,12 @@
+
+class Solution {
+    public String longest(List<String> arr) {
+        String longestStr = "";
+        for (String str : arr) {
+            if (str.length() > longestStr.length()) {
+                longestStr = str;
+            }
+        }
+        return longestStr;
+    }
+}
