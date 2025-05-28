773cada86d28f4bcfd8f1eb3deac6fe73bfc5f1e
Anuj Parihar <131395094+Anuj-Parihar@users.noreply.github.com>
2025-05-28 18:47:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Nearly sorted/nearly-sorted.java
@@ -0,0 +1,48 @@
+// class Solution {
+//     public void nearlySorted(int[] arr, int k) {
+//         // code here
+//         int n= arr.length;
+//       for (int i = 0; i < n - 1; i++) {
+//             for (int j = 0; j < n - 1 - i; j++) {
+//                 if (arr[j] > arr[j + 1]) {
+//                     int temp = arr[j];
+//                     arr[j] = arr[j + 1];
+//                     arr[j + 1] = temp;
+//                 }
+//             }
+//         }
+        
+//     }
+// }
+
+
+class Solution {
+    public void nearlySorted(int[] arr, int K) {
+        quickSort(arr, 0, arr.length - 1);
+    }
+    private void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+    private int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];  
+        int i = low - 1;      
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+}
