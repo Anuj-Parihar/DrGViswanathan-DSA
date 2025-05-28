f3e4819759193dd57c733dcbed95d061d8f6e0cc
Anuj Parihar <131395094+Anuj-Parihar@users.noreply.github.com>
2025-05-28 18:50:28 +0530

    Update nearly-sorted.java

--- Difficulty: Medium/Nearly sorted/nearly-sorted.java
+++ Difficulty: Medium/Nearly sorted/nearly-sorted.java
@@ -18,20 +18,19 @@
 
 class Solution {
     public void nearlySorted(int[] arr, int K) {
-        quickSort(arr, 0, arr.length - 1);
+        quickSort(arr, 0, arr.length-1);
     }
-    private void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
+    private void quickSort(int[]arr, int low, int high) {
+        if (low<high) {
             int pi = partition(arr, low, high);
-            quickSort(arr, low, pi - 1);
+            quickSort(arr, low, pi-1);
             quickSort(arr, pi + 1, high);
         }
     }
     private int partition(int[] arr, int low, int high) {
         int pivot = arr[high];  
-        int i = low - 1;      
-
-        for (int j = low; j < high; j++) {
+        int i = low-1;      
+        for (int j = low;j<high;j++) {
             if (arr[j] <= pivot) {
                 i++;
                 int temp = arr[i];
@@ -39,10 +38,10 @@ class Solution {
                 arr[j] = temp;
             }
         }
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[high];
+        int temp = arr[i +1];
+        arr[i+1] = arr[high];
         arr[high] = temp;
 
-        return i + 1;
+        return i+1;
     }
 }
